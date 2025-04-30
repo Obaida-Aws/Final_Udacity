d1a2d090ca75710bc56002638221150fd5ac69bf
Obaida-Aws <s11923787@stu.najah.edu>
2025-05-01 01:28:10 +0300

    Create first test

---
+++ test/shuffle.test.js
@@ -0,0 +1,23 @@
+import { expect } from "chai";
+import { shuffle } from "../src/shuffle.js";
+
+describe("shuffle function", () => {
+  it("should return a new array with the same length", () => {
+    const cards = [1, 2, 3, 4, 5];
+    const result = shuffle(cards);
+    expect(result).to.have.lengthOf(cards.length);
+  });
+
+  it("should return a new array with the same elements", () => {
+    const cards = ["a", "b", "c", "d"];
+    const result = shuffle(cards);
+    expect(result).to.have.members(cards);
+  });
+
+  it("should not modify the original array", () => {
+    const cards = [1, 2, 3, 4, 5];
+    const copy = [...cards];
+    shuffle(cards);
+    expect(cards).to.deep.equal(copy);
+  });
+});
