a6cf0da3772e6382eabb38b2a39daffe0a00f587
Obaida-Aws <s11923787@stu.najah.edu>
2025-05-01 01:23:50 +0300

    use prettierr

--- src/utilityRenderFunctions.js
+++ src/utilityRenderFunctions.js
@@ -7,7 +7,7 @@ const createElement = (elementType, text) => {
 
 // Creates images with alt
 const createImage = (url, alt) => {
-  const img = document.createElement("img");
+  const img = document.createElement('img');
   img.src = url;
   img.alt = alt;
   return img;
@@ -17,17 +17,17 @@ const createImage = (url, alt) => {
 const createHeader = (headerType, text, dataCy) => {
   const header = document.createElement(headerType);
   header.textContent = text;
-  header.setAttribute("data-cy", dataCy);
+  header.setAttribute('data-cy', dataCy);
   return header;
 };
 
 // Create a button that toggles the visibility of the form
 const createToggleButton = (text, element) => {
-  const button = document.createElement("button");
+  const button = document.createElement('button');
   button.textContent = text;
 
-  button.addEventListener("click", () => {
-    element.classList.toggle("notVisible");
+  button.addEventListener('click', () => {
+    element.classList.toggle('notVisible');
   });
 
   return button;
@@ -37,15 +37,15 @@ const createToggleButton = (text, element) => {
 
 // Helper function to create a label element
 const createLabel = (text, htmlFor) => {
-  const label = document.createElement("label");
+  const label = document.createElement('label');
   label.textContent = text;
-  label.setAttribute("for", htmlFor);
+  label.setAttribute('for', htmlFor);
   return label;
 };
 
 // Helper function to create an input element
 const createInput = (name) => {
-  const input = document.createElement("input");
+  const input = document.createElement('input');
   input.name = name;
   input.id = name;
   return input;
@@ -53,8 +53,8 @@ const createInput = (name) => {
 
 // Helper function to create a submit button
 const createSubmitButton = (value) => {
-  const submit = document.createElement("input");
-  submit.type = "submit";
+  const submit = document.createElement('input');
+  submit.type = 'submit';
   submit.value = value;
   return submit;
 };
