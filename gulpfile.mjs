60df95bcc43f792df912c8234241f75d913a9022
Obaida-Aws <s11923787@stu.najah.edu>
2025-05-01 01:21:09 +0300

    some edits

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -1,11 +1,6 @@
-const gulp = require('gulp');
-const shell = require('gulp-shell');
-
+import gulp from 'gulp';
+import shell from 'gulp-shell';
 
 gulp.task('parcel', shell.task('parcel watch'));
-
-
 gulp.task('test', shell.task('npx cypress run'));
-
-
 gulp.task('default', gulp.series('parcel', 'test'));
