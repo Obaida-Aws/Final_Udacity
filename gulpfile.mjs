b51b547b39b62265823fdbb6fc09c706289c3efb
Obaida-Aws <s11923787@stu.najah.edu>
2025-05-01 17:13:00 +0300

    edit glupfile to put the entry

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -1,6 +1,9 @@
 import gulp from 'gulp';
 import shell from 'gulp-shell';
 
-gulp.task('parcel', shell.task('parcel watch'));
+
+gulp.task('parcel', shell.task('parcel build index.html --dist-dir dist'));
+
 gulp.task('test', shell.task('npx cypress run'));
+
 gulp.task('default', gulp.series('parcel', 'test'));
