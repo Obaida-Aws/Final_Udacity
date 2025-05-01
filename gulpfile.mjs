6f332ad44d87cd383bb5d796221939bbac831863
Obaida-Aws <s11923787@stu.najah.edu>
2025-05-01 18:35:57 +0300

    some edit

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -2,8 +2,10 @@ import gulp from 'gulp';
 import shell from 'gulp-shell';
 
 
-gulp.task('parcel', shell.task('parcel build index.html --dist-dir dist'));
+gulp.task('serve', shell.task('parcel serve index.html --dist-dir dist --port 1234'));
+
 
 gulp.task('test', shell.task('npx cypress run'));
 
-gulp.task('default', gulp.series('parcel', 'test'));
+
+gulp.task('default', gulp.series('serve', 'test'));
